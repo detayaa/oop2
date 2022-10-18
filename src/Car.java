ec7b1de59a2447a10839d9b457098e563c8ac3c0
detayaa <drugochenko.taya@mail.ru>
2022-10-18 21:12:51 +0700

    0

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,22 @@
 public class Car {
+    String brand;
+    String model;
+    double sizeEngine;
+    String color;
+    int date;
+    String country;
+
+
+
+    Car(String brand, int date, String country, String color, double sizeEngine) {
+        this.brand = brand;
+        this.date = date;
+        this.color = color;
+        this.country = country;
+        this.sizeEngine = sizeEngine;
+    }
+
+    void information() {
+        System.out.println(brand + ", " + date + " год выпуска, сборка в " + country + ", " + color + " цвета, объем двигателя - " + sizeEngine + " л." );
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        Car lada = new Car("Lada Granta", 2015, "России", "желтого", 1.7);
+        lada.information();
+        Car audi = new Car("Audi A8 50 L TDI quattro", 2020, "Германии", "черного", 3.0);
+        audi.information();
+        Car bmw = new Car("BMW Z8", 2021, "Гермнии", "черного", 2.4);
+        bmw.information();
+        Car kia = new Car("Kia Sportage 4-го поколения", 2018, "Южной Корее", "красного", 2.4);
+        kia.information();
+        Car hyundai = new Car("Hyundai Avante", 2016, "Южной Корее", "оранжевого", 1.6);
+        hyundai.information();
+
 
     }
 }
