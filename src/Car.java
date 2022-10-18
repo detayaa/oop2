94adb8f40fe4892ac9702b12eaffb6942a0927ea
detayaa <drugochenko.taya@mail.ru>
2022-10-18 23:02:10 +0700

    0

--- src/Car.java
+++ src/Car.java
@@ -8,15 +8,23 @@ public class Car {
 
 
 
-    Car(String brand, int date, String country, String color, double sizeEngine) {
-        this.brand = brand;
+    Car(String brand,String model, int date, String country, String color, double sizeEngine) {
+        if (brand == null) {
+            this.brand = "default";
+        } else { this.brand = brand;}
+        if (model == null) {
+            this.model = "default";
+        } else{ this.model = model;}
+
         this.date = date;
         this.color = color;
-        this.country = country;
+        if (country == null) {
+            this.country = "default";
+        } else { this.country = country;}
         this.sizeEngine = sizeEngine;
     }
 
     void information() {
-        System.out.println(brand + ", " + date + " год выпуска, сборка в " + country + ", " + color + " цвета, объем двигателя - " + sizeEngine + " л." );
+        System.out.println(brand + " " + model + ", " + date + " год выпуска, сборка в " + country + ", " + color + " цвета, объем двигателя - " + sizeEngine + " л." );
     }
 }
